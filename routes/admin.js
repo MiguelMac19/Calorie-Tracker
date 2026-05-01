193eff6d2eb2bb4c32d89274e19da5bc04786246
Ivan Kwok <ivnkwok@gmail.com>
2026-04-30 22:42:46 -0700

    added comments for admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -3,17 +3,29 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 const { requireAdmin } = require('../middleware/auth');
 
+//admin root/dashboard
 router.get('/', requireAdmin, adminController.getDashboard);
+//list custom foods
 router.get('/foods', requireAdmin, adminController.getFoods);
+//retrieve new custom food form
 router.get('/foods/new', requireAdmin, adminController.getNewFood);
+//add new custom global food
 router.post('/foods', requireAdmin, adminController.postNewFood);
+//edit custom global food by id
 router.get('/foods/:id/edit', requireAdmin, adminController.getEditFood);
+//update custom global food
 router.post('/foods/:id', requireAdmin, adminController.postEditFood);
+//delete custom global food
 router.post('/foods/:id/delete', requireAdmin, adminController.deleteFood);
+//retrieve all users
 router.get('/users', requireAdmin, adminController.getUsers);
+//promote user to premium
 router.post('/users/:id/promote', requireAdmin, adminController.promoteUser);
+//promote user to admin
 router.post('/users/:id/make-admin', requireAdmin, adminController.makeAdmin);
+//demote user to free
 router.post('/users/:id/demote', requireAdmin, adminController.demoteUser);
+//deactivate selected user
 router.post('/users/:id/ban', requireAdmin, adminController.toggleBan);
 
 module.exports = router;
